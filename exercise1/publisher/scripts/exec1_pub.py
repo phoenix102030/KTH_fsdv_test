7677375ebf257393dee099bd98507a4bc203f5ff
phoenix102030 <144701236+phoenix102030@users.noreply.github.com>
2024-01-28 15:38:04 +0100

    Add files via upload

---
+++ exercise1/publisher/scripts/exec1_pub.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import Int32
+
+class exec1_pub:
+    def __init__(self):
+        self.pub = rospy.Publisher('/sun', Int32, queue_size=10)
+        rospy.init_node('exec1_pub', anonymous=True)
+        self.rate = rospy.Rate(20) # 20hz
+        self.n = 4
+        self.k = 0
+
+    def run(self):
+        while not rospy.is_shutdown():
+            self.k = self.k + self.n
+            rospy.loginfo("Publishing: %d", self.k)
+            self.pub.publish(self.k)
+            self.rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        node = exec1_pub()
+        node.run()
+    except rospy.ROSInterruptException:
+        pass
---
+++ exercise1/subscriber/scripts/exec1_sub.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import Int32, Float32
+
+class exec1_sub:
+    def __init__(self):
+        rospy.init_node('exec1_sub', anonymous=True)
+        self.publisher = rospy.Publisher('/kthfs/result', Float32, queue_size=10)
+        rospy.Subscriber('/sun', Int32, self.callback)
+
+    def callback(self, data):
+        q = 0.15
+        result = data.data / q
+        rospy.loginfo('Result: %f', result)
+        self.publisher.publish(result)
+
+    def run(self):
+        rospy.spin()
+
+if __name__ == '__main__':
+    try:
+        node = exec1_sub()
+        node.run()
+    except rospy.ROSInterruptException:
+        pass
