5dd340ac6b36a3e484f27feb8b87b69e06d7868c
phoenix102030 <144701236+phoenix102030@users.noreply.github.com>
2024-01-28 16:37:03 +0100

    Update visualisation.py

--- kthfsdv-plotting/visualisation.py
+++ kthfsdv-plotting/visualisation.py
@@ -1,83 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import datetime
-from matplotlib.widgets import Slider, Button, TextBox
 
-
-class visualizer:
-    def __init__(self):
-        self.fig, self.ax = plt.subplots()
-        self.l, = self.ax.plot([0, 10], [-10,1000], 'b', label='h(t)')  
-        self.ax.grid(True)
-        self.ax.legend()
-
+class SimpleVisualizer:
+    def __init__(self, t_start=0, t_end=10, t_interval=0.1):
+        self.t = np.arange(t_start, t_end, t_interval)
         self.lambda_func = lambda t: 5 * np.sin(2 * np.pi * 1 * t)
-        self.time_interval = 0.1
-        self.t = np.arange(0, 10, self.time_interval)
+        self.h_func = lambda t: 3 * np.pi * np.exp(-self.lambda_func(t))
+        self.fig, self.ax = plt.subplots()
 
-        # adjusting axis with slider (zoom functionality)
-        self.axcolor = 'lightgray'
-        ax_lambda = plt.axes([0.12, 0.01, 0.65, 0.03], facecolor=self.axcolor)
-        self.lambda_slider = Slider(ax_lambda, 'Lambda', 1, 10, valinit=4)
-        self.lambda_slider.on_changed(self.update)
+    def plot(self):
+        h = self.h_func(self.t)
+        self.ax.plot(self.t, h, label='h(t)')
+        self.ax.legend()
+        plt.show()
 
-        ax_reset = plt.axes([0.9, 0.2, 0.1, 0.04])
-        self.reset_button = Button(ax_reset, 'Reset', color=self.axcolor, hovercolor='0.7')
-        self.reset_button.on_clicked(self.reset)
-
-        ax_save = plt.axes([0.9, 0.25, 0.1, 0.04])
-        self.save_button = Button(ax_save, 'Save', color=self.axcolor, hovercolor='0.7')
-        self.save_button.on_clicked(self.save_data)
-
-        # input experiment name
-        ax_textbox = plt.axes([0.3, 0.92, 0.2, 0.04])
-        self.textbox = TextBox(ax_textbox, 'Experiment Name: ')
-        self.textbox.on_submit(self.set_experiment_name)
-
-        self.update(None)
-
-    def update_data(self):
-        self.h = 3 * np.pi * np.exp(-self.lambda_func(self.t))
-
-    def update(self, val):
-        self.lambda_func = lambda t: self.lambda_slider.val * np.sin(2 * np.pi * 1 * t)
-        self.update_data()
-        self.l.set_data(self.t, self.h)
-        self.fig.canvas.draw_idle()
-
-    def reset(self, event):
-        self.t = np.arange(0, 10, self.time_interval)
-        self.lambda_slider.set_val(5)
-        self.update_data()
-        self.l.set_data(self.t, self.h)
-        self.fig.canvas.draw_idle()
-
-    def set_experiment_name(self, text):
-        self.experiment_name = text
-
-    def save_data(self, event):
-        timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-        filename = f"{timestamp}_{self.experiment_name}.csv"
-        np.savetxt(filename, np.column_stack((self.t, self.h)), delimiter=",", header="Time,h(t)", comments="")
-
-if __name__ == "__main__":
-    v = visualizer()
-    plt.show()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+vis = SimpleVisualizer()
+vis.plot()
