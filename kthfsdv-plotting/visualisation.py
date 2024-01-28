42b2da517856c1fabf90b28634518b9abd141565
phoenix102030 <144701236+phoenix102030@users.noreply.github.com>
2024-01-28 16:43:45 +0100

    Update visualisation.py

--- kthfsdv-plotting/visualisation.py
+++ kthfsdv-plotting/visualisation.py
@@ -2,12 +2,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class SimpleVisualizer:
-    def __init__(self, t_start=0, t_end=10, t_interval=0.1):
+    def __init__(self, t_start=0, t_end=5, t_interval=0.1):
         self.t = np.arange(t_start, t_end, t_interval)
-        self.lambda_func = lambda t: 5 * np.sin(2 * np.pi * 1 * t)
-        self.h_func = lambda t: 3 * np.pi * np.exp(-self.lambda_func(t))
         self.fig, self.ax = plt.subplots()
 
+    def lambda_func(self, t):
+        return 5 * np.sin(2 * np.pi * 1 * t)
+
+    def h_func(self, t):
+        return 3 * np.pi * np.exp(-self.lambda_func(t))
+        
     def plot(self):
         h = self.h_func(self.t)
         self.ax.plot(self.t, h, label='h(t)')
